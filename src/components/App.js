0196c5a0ced98a213e9ae2d2059dd58f66ae2b75
aditya492 <adityabarve96@gmail.com>
2021-06-24 12:07:30 +0530

    Editor

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import Editor from "./Editor";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+function App() {
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `);
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="Code Here"
+          value={html}
+          onChange={setHtml}
+        />
+
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="1"
+          width="50%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
